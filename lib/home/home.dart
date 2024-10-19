f5b4ffb8b6ed126c2dcc78983cde6a37e90d023a
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2024-10-18 19:32:53 -0600

    finish login

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,11 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void _logout(BuildContext context) async {
+    try {
+      await _auth.signOut();
+      // After logout, redirect to the login page
+      Navigator.of(context).pushReplacementNamed(
+          '/login'); // Ensure you have this route setup for your login page
+    } catch (e) {
+      print("Logout Error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Logout Failed: ${e.toString()}')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
-      body: Center(child: Text('Welcome to the Campfire!')),
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => _logout(context),
+            tooltip: 'Logout',
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Home Page!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
